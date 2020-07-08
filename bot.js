4a73ba4f1cbe93e83e584982a6fef9bdf9b154ae
Ira Justus Fesefeldt <ira@feuervogel.me>
2020-07-08 22:13:41 +0200

    init

---
+++ bot.js
@@ -0,0 +1,46 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+var countdown = new Date(1593968400000);
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'howLong':
+				var timeLeft = (countdown - Date.now()) / 1000;
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Its '+ timeLeft + ' seconds remaining.'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
