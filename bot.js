05fde1347d21b08b63c04bc578cde5a3f5738ad4
Ira Justus Fesefeldt <ira@feuervogel.me>
2020-07-12 16:08:47 +0200

    added commander tool

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var Discord = require('discord.js');
 var logger = require('winston');
 var moment = require('moment');
 var momentTimezone = require('moment-timezone');
+var commander = require('bot-commander');
 var auth = require('./auth.json');
 
 
@@ -22,97 +23,64 @@ timezoneAbbr.set('PST','-8');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+	colorize: true
 });
 logger.level = 'debug';
 
 // Initialize Discord Bot
 var bot = new Discord.Client();
-
 bot.on('ready', onReady);
 bot.on('message', onMessage);
-
 bot.login(auth.token);
 
+
+//Initialize Commander
+let doCmd = commander.command('.do').showHelpOnEmpty();
+doCmd.command('now').description('Shows current time.').action(handleNow);
+doCmd.command('load <name>').description('Loads the time from your database').action(handleLoad);
+doCmd.command('date <date>').description('Shows the input date with the format DD-MM-YYYY').action(handleDate);
+doCmd.command('time <time>').description('Shows the input time with the format HH:MM:SS:ms').action(handleTime);
+doCmd.command('datetime <date> <time>').description('Shows the input date and time with the format DD-MM-YYYY HH:MM(:SS(:ms))').action(handleDateTime);
+doCmd.option('countdown-title', 'If the remaining time should be presented in the title as a countdown', countdown-title, false);
+doCmd.option('countdown', 'If the remaining time should be presented in this channel as a countdown', countdown, false);
+doCmd.option('as <format>', 'Can be used to demonstrate the format on how the countdown/time should be displayed.', format, 'LLLL');
+doCmd.option('to <timezone', 'To which timezone the time should be displayed', timezone, 'Etc/GMT+0');
+doCmd.option('save <name>', 'Saves this time in your database', save);
+
+commander.command('.locale <locale').description('Changes the locale of this server').action(changeLocale);
+
 function onReady() {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.user.username + ' - (' + bot.user.id + ')');
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.user.username + ' - (' + bot.user.id + ')');
 }
 
 function onMessage(message) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `.`
-    var text = message.cleanContent;
-    if (text.substring(0, 1) == '.') {
-        var args = text.substring(1).split(' ');
-        var cmd = args[0];
-        args = args.splice(1);
-        
-        logger.info('locale: '+message.author.locale);
-        
-        moment.locale(message.author.locale);
-	
-        switch (cmd) {
-            //sets the language for this server
-            case 'lang':
-        
-            // outputs into the chat the current ime
-            case 'time':
-            	printCurrentTime(message.channel,args);
-            	break;
-
-                // save a time in the database
-            case 'save':
-
-                // output a saved time into the chat
-            case 'load':
-
-                // output how much time is left until a saved time into the chat
-            case 'long':
-
-                // give a countdown to a saved time into the chat
-            case 'countdown':
-
-                // give a countdown to a saved time into the chat title
-            case 'countdownTitle':
-            
-            case 'is-Ira-a-Warlock?':
-            	message.channel.send('Yes.');
-            	break;
-            
-            default:
-            	message.channel.send('No.');
-
-
-                break;
-                // Just add any case commands if you want to..
-        }
-    }
+	message.channel.send(commander.parse(message.cleanContent,message));
 }
 
 
 function printCurrentTime(channel,args){
-    var timezone = '';
-    var form = 'LLL';
-    if(args.length > 0){
-    	timezone = args[0];
-    }
-    if(args.length > 1){
-    	form = args[1];
-    }
+	var timezone = '';
+	var form = 'LLL';
+	if(args.length > 0){
+		timezone = args[0];
+	}
+	if(args.length > 1){
+		form = args[1];
+	}
     
-    var date;
-    if(timezoneAbbr.has(timezone)){
-        var offset = timezoneAbbr.get(timezone);
-        date = moment().utcOffset(offset*60);
-    } else if(!isNaN(parseInt(timezone))){
-    	date = moment().utcOffset(parseInt(timezone))
-    } else {
-    	date = moment.tz(timezone);
-    }
+	var date;
+	if(timezoneAbbr.has(timezone)){
+		var offset = timezoneAbbr.get(timezone);
+		date = moment().utcOffset(offset*60);
+	} else if(!isNaN(parseInt(timezone))){
+		date = moment().utcOffset(parseInt(timezone))
+	} else {
+		date = moment.tz(timezone);
+	}
    
     
-    channel.send(date.format(form));
+	channel.send(date.format(form));
 }
 
